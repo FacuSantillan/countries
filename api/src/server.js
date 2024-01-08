28b87d1bf84203baf55cc2aa0f8b4fd24a69d224
Facundo <facusantillan1@gmail.com>
2024-01-08 15:55:32 -0300

    j

--- api/src/server.js
+++ api/src/server.js
@@ -10,7 +10,7 @@ server.use(bodyParser.urlencoded({ extended: true, limit: '50mb' }));
 server.use(bodyParser.json({ limit: '50mb' }));
 
 server.use((req, res, next) => {
-  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Origin', '*', 'http://localhost:5173/');
   res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, DELETE');
   res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization');
   next();

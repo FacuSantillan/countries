907650576b9e9ab287a70071efe95e021185377a
Facundo <facusantillan1@gmail.com>
2024-01-08 15:37:41 -0300

    j

--- api/src/server.js
+++ api/src/server.js
@@ -1,21 +1,21 @@
-const router = require("./routes");
-const cors = require("cors");
 const express = require('express');
+const cors = require('cors');
 const bodyParser = require('body-parser');
-const morgan = require('morgan');
+const router = require('./routes');
+
 const server = express();
 
-server.use(cors())
+server.use(cors());
 server.use(bodyParser.urlencoded({ extended: true, limit: '50mb' }));
 server.use(bodyParser.json({ limit: '50mb' }));
+
 server.use((req, res, next) => {
-  res.header('Access-Control-Allow-Origin', '*'); // Permitir cualquier origen
-  res.header('Access-Control-Allow-Credentials', 'true');
-  res.header('Access-Control-Allow-Headers', "apprend,delete,entries,foreach,get,has,keys,set,values,Authorization");
+  res.header('Access-Control-Allow-Origin', '*');
   res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, DELETE');
-  res.header('HTTP/1.1 200 OK')
+  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization');
   next();
 });
+
 server.use(router);
 
 module.exports = server;
